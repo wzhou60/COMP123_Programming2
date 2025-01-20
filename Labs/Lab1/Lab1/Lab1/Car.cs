d46a8fff1b2ed0ea73cd2aaae1914d17d057894b
dillcheese <dillcheese1@gmail.com>
2025-01-20 11:33:08 -0500

    init

---
+++ Labs/Lab1/Lab1/Lab1/Car.cs
@@ -0,0 +1,25 @@
+﻿namespace Lab1
+{
+    class Car
+    {
+        private int year;
+        private string manufacturer;
+        private string model;
+        private double price;
+        private bool isDriveable;
+
+        public Car(int year, string manufacturer, string model, double price, bool isDriveable = true)
+        {
+            this.year = year;
+            this.manufacturer = manufacturer;
+            this.model = model;
+            this.price = price;
+            this.isDriveable = isDriveable;
+        }
+
+        public override string ToString()
+        {
+            return "Year: " + year + ", Manufacturer: " + manufacturer + ", Model: " + model + ", Price: " + price + ", Driveable: " + isDriveable;
+        }
+    }
+}
---
+++ Labs/Lab1/Lab1/Lab1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Lab1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Car myCar1 = new Car(2028, "Honda", "Civic", 30000.00, true);
+            Car myCar2 = new Car(2020, "Toyota", "Sprinter", 35000.00, true);
+            Car myCar3 = new Car(2012, "Tesla", "F150", 40000.00, true);
+            Car myCar4 = new Car(2013, "Ford", "Model Y", 450000.00, false);
+            Car myCar5 = new Car(2013, "Audi", "Corolla", 650000.00);
+            Console.WriteLine(myCar1.ToString());
+            Console.WriteLine(myCar2.ToString());
+            Console.WriteLine(myCar3.ToString());
+            Console.WriteLine(myCar4.ToString());
+            Console.WriteLine(myCar5.ToString());
+        }
+    }
+}
